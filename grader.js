cd077e08d85c2d27183d1ff403dbfd0a34927680
Ubuntu <ubuntu@ip-172-31-18-137.us-west-2.compute.internal>
2013-07-11 03:21:09 +0000

    Added url option

--- grader.js
+++ grader.js
@@ -26,6 +26,8 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
+var htmlfile = "urlfile.html" ;
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL Path')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    rest.get(program.url)
+        .on('complete',function(response){
+            fs.writeFileSync(htmlfile,response);
+            var checkJson = checkHtmlFile(htmlfile,program.checks);
+            var outJson = JSON.stringify(checkJson, null, 4);
+ //           exports.outJson=outJson ;
+            console.log(outJson);
+        })
+        .on('error',function(err){
+            console.log('An Error Occured:'+err);
+        });
+
+//    var checkJson = checkHtmlFile(program.file, program.checks);
+//    var outJson = JSON.stringify(checkJson, null, 4);
+//    console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
